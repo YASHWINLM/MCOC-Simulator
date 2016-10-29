0f5edad20ff619b6fef0463f50627ca3bc7f5efa
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-10-29 12:20:59 -0700

    MCOC SIMULATOR 4*&5*

---
+++ Assets/Scripts/CrystalController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalController : MonoBehaviour {
+	void Update () {
+		transform.Rotate (new Vector3(0,5,0));	
+	}
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+	public int Stars;
+	public string Name;
+	public int PI;
+
+	void Update(){
+		transform.position += new Vector3 (0.01f,0f,0f);
+		if(transform.position.x>1){
+			GameObject heroObject = Instantiate (Resources.Load("Prefabs/Hero"), new Vector3(-1f, -0.5f,2f), Quaternion.Euler(new Vector3(0,0,180))) as GameObject;
+			Hero hero = heroObject.GetComponent<Hero> ();
+			int i = Random.Range (0,4);
+
+			MeshRenderer renderer = heroObject.GetComponent<MeshRenderer> ();
+			if(i==0){
+				renderer.material = Resources.Load ("Materials/meme") as Material;
+			}
+			else if(i==1){
+				renderer.material = Resources.Load ("Materials/keke") as Material;
+			}
+			if(i==2){
+				renderer.material = Resources.Load ("Materials/danke") as Material;
+			}
+			if(i==3){
+				renderer.material = Resources.Load ("Materials/lit") as Material;
+			}
+			Destroy (this.gameObject);
+		}
+	}
+}
