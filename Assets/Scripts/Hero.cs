15064f03946b90e00ba4efae10ba6cffb3ca147b
gerings17 <skyswims@yahoo.com>
2016-12-03 12:07:14 -0800

    Made the crystal stop

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,29 +4,37 @@ using System.Collections;
 public class Hero : MonoBehaviour {
 	public int Stars;
 	public string Name;
-	public int PI;
-
+	public static float HeroSpeed=0.01f;
 	void Update(){
-		transform.position += new Vector3 (0.01f,0f,0f);
+		transform.position += new Vector3 (HeroSpeed,0f,0f);
 		if(transform.position.x>1){
 			GameObject heroObject = Instantiate (Resources.Load("Prefabs/Hero"), new Vector3(-1f, -0.5f,2f), Quaternion.Euler(new Vector3(0,0,180))) as GameObject;
 			Hero hero = heroObject.GetComponent<Hero> ();
-			int i = Random.Range (0,4);
+			int i = Random.Range (0,5);
 
-			MeshRenderer renderer = heroObject.GetComponent<MeshRenderer> ();
+
 			if(i==0){
-				renderer.material = Resources.Load ("Materials/meme") as Material;
+				SetMaterial (heroObject, "Hawkeye");
 			}
 			else if(i==1){
-				renderer.material = Resources.Load ("Materials/keke") as Material;
+				SetMaterial (heroObject, "Hulk Buster");
 			}
 			if(i==2){
-				renderer.material = Resources.Load ("Materials/danke") as Material;
+				SetMaterial (heroObject, "Antman");
 			}
 			if(i==3){
-				renderer.material = Resources.Load ("Materials/lit") as Material;
+				SetMaterial (heroObject, "Unst.");
 			}
+			if(i==4){
+				SetMaterial (heroObject, "venom");
+			}
+		
 			Destroy (this.gameObject);
 		}
+	}
+	void SetMaterial(GameObject target, string path){
+		MeshRenderer renderer = target.GetComponent<MeshRenderer> ();
+		renderer.material = Resources.Load ("Materials/" + path) as Material;
+		target.GetComponent<Hero>().Name = path;
 	}
 }
---
+++ Assets/Scripts/SpinController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SpinController : MonoBehaviour {
+	void Start () {
+		StartCoroutine (SlowLater ());
+	}
+
+	public IEnumerator SlowLater(){
+		yield return new WaitForSeconds (7);
+		while (Hero.HeroSpeed >= 0f) {
+			yield return new WaitForSeconds (UnityEngine.Random.Range(0.09f, 0.11f));
+			Hero.HeroSpeed -= 0.0001f;
+		}
+		Vector3 pos = Camera.main.transform.position;
+		pos.y -= 0.25f;
+
+		Hero.HeroSpeed = 0;
+		RaycastHit x;
+		if (Physics.Raycast (pos, Vector3.forward, out x)) {
+			Debug.Log (x.transform);
+			Hero hit = x.transform.GetComponent<Hero> ();
+			Debug.Log (hit.Name);
+		}
+		else {
+			pos.x -= 0.25f;
+			if (Physics.Raycast (pos, Vector3.forward, out x)) {
+				Debug.Log (x.transform);
+				Hero hit = x.transform.GetComponent<Hero> ();
+				Debug.Log (hit.Name);
+			}
+			foreach (GameObject hero in GameObject.FindGameObjectsWithTag("Hero" )) {
+				hero.transform.position += new Vector3(-0.25f, 0f, 0f);
+			}
+		}
+	}
+}
